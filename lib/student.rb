ab32875c643daedd5d9285f24bf201f9440b737a
Peyton <pekellogg@hotmail.com>
2022-03-09 20:21:57 -0800

    commit tree 288b7d8d03cfdace463f6cc8cfda5431d256b3ac

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,6 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    #dynamic readers + writers: iterate over the return value of #column_names with #attr_accessor
+    self.column_names.each { |col_name| attr_accessor col_name.to_sym}
 end
